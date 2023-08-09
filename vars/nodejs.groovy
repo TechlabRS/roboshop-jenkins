6127b281a92d99477a0a92fd86e17398478c4188
TechlabRS <129305203+TechlabRS@users.noreply.github.com>
2023-08-09 14:02:23 +0530

    upload for june07

---
+++ vars/nodejs.groovy
@@ -0,0 +1,54 @@
+def call() {
+  pipeline {
+
+    agent {
+      node {
+        label 'workstation'
+      }
+    }
+
+    options {
+      ansiColor('xterm')
+    }
+
+
+    stages {
+
+      stage('Code Quality') {
+        steps {
+          sh 'ls -l'
+          sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.8.27:9000 -Dsonar.login=admin -Dsonar.password=admin123'
+        }
+      }
+
+      stage('Unit Test Cases') {
+        steps {
+          sh 'echo Unit tests'
+        }
+      }
+
+      stage('CheckMarx SAST Scan') {
+        steps {
+          sh 'echo Checkmarx Scan'
+        }
+      }
+
+      stage('CheckMarx SCA Scan') {
+        steps {
+          sh 'echo Checkmarx SCA Scan'
+        }
+      }
+
+
+    }
+
+    post {
+      always {
+        cleanWs()
+      }
+    }
+
+  }
+
+
+}
